adcbe90d2abccd573ebcf728f0eb8c01b9d701a1
dbouget <bougetd@gmail.com>
2022-05-11 16:27:06 +0200

    windows tf sse2 compatible.

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ with open("README.md", "r", errors='ignore') as f:
 with open('requirements.txt', 'r', encoding='utf-8', errors='ignore') as ff:
     required = ff.read().splitlines()
 
-required.append('tensorflow==1.13.1')
+if platform.system() == 'Windows':
+    required.append('tensorflow@https://github.com/andreped/tensorflow-windows-wheel/raw/master/1.13.1/py37/CPU/sse2/tensorflow-1.13.1-cp37-cp37m-win_amd64.whl')
+else:
+    required.append('tensorflow==1.13.1')
 
 
 setup(
