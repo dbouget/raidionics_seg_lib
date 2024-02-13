e4a8f6bbc70798b79095c1fbeb02cc7b85e01974
dbouget <bougetd@gmail.com>
2024-02-13 14:20:32 +0100

    Deprecating Python 3.7, moving the ARM CI away from self-hosted

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         ]
     },
     install_requires=required,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     version='1.2.2',
     author='David Bouget (david.bouget@sintef.no)',
     license='BSD 2-Clause',
