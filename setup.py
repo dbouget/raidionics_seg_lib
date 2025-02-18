6eddfaad830828af0fcdf891c411c7f4ced3ecb1
dbouget <david.bouget@sintef.no>
2025-02-18 12:17:35 +0000

    Fixing the full volume inference for pytorch models [skip ci]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     },
     install_requires=required,
     python_requires=">=3.8",
-    version='1.3.1',
+    version='1.4.0',
     author='David Bouget (david.bouget@sintef.no)',
     license='BSD 2-Clause',
     description='Raidionics segmentation and classification back-end with ONNX runtime',
