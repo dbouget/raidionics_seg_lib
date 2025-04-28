4f002903dc594d1b35992bd4588f00fcdae61ef1
dbouget <bougetd@gmail.com>
2025-04-28 11:19:01 +0200

    Adding new universal build and classification tests [skip ci]

--- tests/conftest.py
+++ tests/conftest.py
@@ -92,3 +92,40 @@ def data_test3(temp_dir):
         shutil.rmtree(dest_dir)
         raise ValueError("Error during resources download.\n")
     return dest_dir
+
+@pytest.fixture(scope="session")
+def data_test_classification_neuro(temp_dir):
+    try:
+        test_image_url = 'https://github.com/raidionics/Raidionics-models/releases/download/1.2.0/Samples-RaidionicsSegLib-UnitTest2.zip'
+        test_model_url = 'https://github.com/raidionics/Raidionics-models/releases/download/v1.3.0-rc/Raidionics-MRI_SequenceClassifier-v13.zip'
+        dest_dir = os.path.join(temp_dir, "Test_classif_neuro")
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
+        os.makedirs(dest_dir)
+
+        archive_dl_dest = os.path.join(dest_dir, 'inference_volume.zip')
+        headers = {}
+        response = requests.get(test_image_url, headers=headers, stream=True)
+        response.raise_for_status()
+        if response.status_code == requests.codes.ok:
+            with open(archive_dl_dest, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1048576):
+                    f.write(chunk)
+        with zipfile.ZipFile(archive_dl_dest, 'r') as zip_ref:
+            zip_ref.extractall(dest_dir)
+
+        archive_dl_dest = os.path.join(dest_dir, 'classif_model.zip')
+        headers = {}
+        response = requests.get(test_model_url, headers=headers, stream=True)
+        response.raise_for_status()
+        if response.status_code == requests.codes.ok:
+            with open(archive_dl_dest, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1048576):
+                    f.write(chunk)
+        with zipfile.ZipFile(archive_dl_dest, 'r') as zip_ref:
+            zip_ref.extractall(dest_dir)
+    except Exception as e:
+        logging.error("Error during resources download with: \n {}.\n".format(traceback.format_exc()))
+        shutil.rmtree(dest_dir)
+        raise ValueError("Error during resources download.\n")
+    return dest_dir
