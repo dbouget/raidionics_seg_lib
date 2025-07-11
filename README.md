81ded2811431bcad93b594d1080cb3ac1ea55883
David Bouget <dbouget@users.noreply.github.com>
2025-07-11 10:16:37 +0200

    Cleaner tests (#14)
    * Polishing the integration tests for more robustness [skip ci]

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Raidionics backend for segmentation and classification
 
 [![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
-[![](https://img.shields.io/badge/python-3.8|3.9|3.10|3.11-blue.svg)](https://www.python.org/downloads/)
+[![](https://img.shields.io/badge/python-3.9|3.10|3.11|3.12|3.13-blue.svg)](https://www.python.org/downloads/)
 [![Paper](https://zenodo.org/badge/DOI/10.3389/fneur.2022.932219.svg)](https://www.frontiersin.org/articles/10.3389/fneur.2022.932219/full)
 [![codecov](https://codecov.io/gh/dbouget/raidionics_seg_lib/branch/master/graph/badge.svg?token=ZSPQVR7RKX)](https://codecov.io/gh/dbouget/raidionics_seg_lib)
 [![PyPI version](https://img.shields.io/pypi/v/raidionicsseg.svg)](https://pypi.org/project/raidionicsseg/)
--- tests/generic_tests/test_inference_segmentation_reconstruction.py
+++ tests/generic_tests/test_inference_segmentation_reconstruction.py
@@ -123,8 +123,11 @@ def test_inference_segmentation_reconstruction_method(test_dir):
             segmentation_pred_filename = os.path.join(output_folder, 'pred_Brain.nii.gz')
             assert os.path.exists(segmentation_pred_filename), "Inference CLI test failed, no brain mask was generated.\n"
             segmentation_gt_filename = os.path.join(test_dir, 'Inputs', 'PreopNeuro', 'verif', 'input0_pred_Brain.nii.gz')
+            logging.info(f"Comparing {segmentation_pred_filename} with original {segmentation_gt_filename}")
             segmentation_pred = nib.load(segmentation_pred_filename).get_fdata()[:]
             segmentation_gt = nib.load(segmentation_gt_filename).get_fdata()[:]
+            logging.info(
+                f"Ground truth and prediction arrays difference: {np.count_nonzero(abs(segmentation_gt - segmentation_pred))} pixels")
             # assert np.array_equal(segmentation_pred, segmentation_gt), "Ground truth and prediction arrays are not identical"
         except Exception as e:
             logging.error(f"Error during inference Python package test with: {e} \n {traceback.format_exc()}.\n")
